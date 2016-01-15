f8cba03238f0f722d92ee26f29ff9b6aeb681a95
Joey <joeyarnstein@gmail.com>
2016-01-15 15:37:09 -0800

    add more destinations and asign a destination to all possible outcomes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,96 @@
 $(document).ready(function() {
   $("#infoInput form").submit(function(event) {
+    
     var familySelect = $("select#family").val();
     var excitementSelect = $("select#excitement").val();
     var ageInput = parseInt($("input#age").val());
     var arrayInputs = [familySelect, excitementSelect, ageInput];
-    debugger;
 
-  //  if (familySelect === "alone" && ) {
-  //  }
+    if (familySelect === "kids") {
+      /* All rules for kids */
+      if (excitementSelect === "beach") {
+        /* beach */
+        if (ageInput > 50) {
+          $("#florida").show();
+          $("body").css('background-color','yellow');
+        }
+        else
+        {
+          $("#jamaica").show();
+          $("body").css('background-color','green');
+        }
+      } else if (excitementSelect === "party") {
+        /* club hopper */
+        if (ageInput > 35) {
+          $("#florida").show();
+          $("body").css('background-color','yellow');
+        } else {
+          $("#vegas").show();
+          $("body").css('background-color','#da80fb');
+        }
+      } else {
+        /* home-body */
+          $("#whocares").show();
+      }
+      /* end rules for kids --------------
+      -start rules for Friends */
+    } else if (familySelect === "friend") {
+      /* All rules for friends */
+      if (excitementSelect === "beach") {
+        /* beach */
+        if (ageInput > 50) {
+          $("#florida").show();
+          $("body").css('background-color','yellow');
+        }
+        else
+        {
+          $("#jamaica").show();
+          $("body").css('background-color','green');
+        }
+      } else if (excitementSelect === "party") {
+        /* club hopper */
+        if (ageInput > 50) {
+          $("#florida").show();
+          $("body").css('background-color','yellow');
+        } else {
+          $("#whocares").show();
+          $("body").css('background-color','#da80fb');
+        }
+      } else {
+        /* home-body */
+        $("#jonestown").show();
+        $("body").css('background-color', 'pink')
+      }
+      /* end rules for friends traveling */
+    } else {
+      /* Rules for traveling alone */
+      if (excitementSelect === "beach") {
+        /* beach */
+        if (ageInput > 50) {
+          $("#florida").show();
+          $("body").css('background-color','yellow');
+        }
+        else
+        {
+          $("#jamaica").show();
+          $("body").css('background-color','green');
+        }
+      } else if (excitementSelect === "party") {
+        /* club hopper */
+        if (ageInput > 35) {
+          $("#florida").show();
+          $("body").css('background-color','yellow');
+        } else {
+          $("#whocares").show();
+          $("body").css('background-color','#da80fb');
+        }
+      } else {
+        /* home-body */
+        $("#jonestown").show();
+        $("body").css('background-color', 'pink')
+      }
+    }
 
-  //  event.preventDefault();
+    event.preventDefault();
   });
 });
